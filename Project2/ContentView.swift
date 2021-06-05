cc0b706aaa90c7b9fdeb63b9ed5c0c042f6e684c
Vio <violetta.azina@yandex.ru>
2021-06-05 09:46:09 +0300

    creat commit

--- Project2/ContentView.swift
+++ Project2/ContentView.swift
@@ -8,14 +8,90 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var email = ""
+    @State var pass = ""
+    @State var log = ""
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
-}
+        
+        ZStack{// главный стек в нем будут храниться другие стеки
+            
+            VStack{// вертикальный стек для текста
+                
+                Image("qqq")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 200, height: 200, alignment: .center)
+                    .padding(.vertical,40)
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+                //выравнивание текста емаил и пароль по левому краю
+                VStack(alignment: .leading) {
+            }
+                    Text("EMAIL")
+                        .font(
+                        .custom("Areal", size: 15))//изменение шрифта и его размер
+                        .foregroundColor(.black)
+                        .padding(.trailing, 5)
+                
+                HStack{
+                    Image(systemName: "envelope")
+                        .padding(.leading)
+                    
+                        TextField("name@email.com", text: $email)//делаем рамочку для ввода текста
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .padding(.horizontal,5)
+                 /////////////////////////////////////////////////////////////////////////////////////
+                    
+                    VStack(alignment: .leading){
+                Text("PASSWORD")
+                        .font(
+                        .custom("Areal", size: 15))//изменение шрифта и его размер
+                        .foregroundColor(.black)
+                        .padding(.trailing, 35)
+                   
+
+                VStack{
+                TextField("********", text: $pass)//делаем рамочку для ввода текста
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding(.horizontal,5)
+                 
+                        ////////////////////////////////////////////////////////////////////////////
+
+                   
+                    Button(action: {
+                    email = email
+                    //pass=passs
+                        
+                    },label: {
+                    Text("SIGN UP")
+                        .foregroundColor(.white)
+                        .frame(width: 380, height: 50, alignment: .center)
+                        .background(Color.blue)
+                        .cornerRadius(30)
+                })
+                    .padding(.vertical,50)
+                }
+                    Button(action: {
+                        log=log
+                    }, label: {
+                    Text("Login")
+                    .foregroundColor(.blue)
+                })
+                    .padding(.horizontal,150)
+            }
+                        
+        }
+                    
+                }
+            }
+        }
     }
-}
+
+ struct ContentView_Previews: PreviewProvider {
+                    static var previews: some View {
+                        ContentView()
+            }
+    
+ }
+                    
+               
